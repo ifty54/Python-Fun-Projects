a87d272ac24334cf53e760e74d7b49d26c29b393
Al-Amin Ifty <ifty.alamin54@gmail.com>
2023-07-24 01:19:54 +0600

    Create Guessing Game.py

---
+++ Guessing Game.py
@@ -0,0 +1,21 @@
+import random
+
+secret_number = random.randint(1,10)
+
+#prompt users to guess it
+guess = int(input("enter your guess: "))
+
+#keep track of the guess
+number_guess = 1
+
+#keeping asking user to guess until they make it
+while guess != secret_number:
+    if guess > secret_number:
+        print("too high")
+    else:
+        print("too low")
+    guess = int(input("enter again your guess: "))
+    number_guess += 1
+
+#finally guessed it right
+print(f"You've guessed it right after {number_guess} tries. The correct number is {guess}")
